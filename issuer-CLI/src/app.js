bb5c61ffe0f4d875afd0193f62dd46b0b653af03
Tomasz Gąsior <tomek.mesh@gmail.com>
2019-05-20 14:28:29 +0200

    add issuer CLI

---
+++ issuer-CLI/src/app.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const program = require('commander')
+program
+  .version('0.0.1')
+  .description('Issuer CLI')
+  .command('ico', 'create Stellar token ICO').alias('i')
+  .command('freeze', 'freeze token').alias('f')
+  .command('unfreeze', 'unfreeze token').alias('x')
+  .command('balance', 'get asset balance').alias('b')
+  .parse(process.argv);

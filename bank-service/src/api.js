484c26a7826f7a5fa9d5dec549cf689b82fccde0
Tomasz Gąsior <tomek.mesh@gmail.com>
2019-05-27 11:59:22 +0200

    Add catchify lib

--- bank-service/src/api.js
+++ bank-service/src/api.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const catchify = require('catchify')
 const axios = require('axios')
 
 const stellarUtils = require('./stellar_utils')
@@ -10,81 +11,65 @@ api.post('/ico', async (req, res) => {
   console.log(`ICO KURWA`)
   const {token, sponsorSeed, amount} = req.body;
 
-  const result = await queries.selectToken(token)
+  const [errorQuery, tokenRows] = await catchify(queries.selectToken(token))
 
-  if (!result) {
+  if (errorQuery) {
     console.log('error DB')
     return res.status(500).send('DB error')
   }
 
-  if (result.rows.length !== 0) {
-    console.log(`Token ${token} exists`);
+  if (tokenRows.rows.length !== 0) {
+    console.log(`Token ${token} already exists`);
     return res.status(201).send(`Token ${token} exists`)
   }
 
   // create issuer
-  let issuer
-  try {
-    issuer = await stellarUtils.fundAccount(`issuer${token}`, sponsorSeed)
-  }
-  catch (error) {
-    console.log(`Error creating issuer: ${error}`);
-    return res.status(501).send(error)
+  const [error1, issuer] = await catchify(stellarUtils.fundAccount(`issuer${token}`, sponsorSeed))
+  if (error1) {
+    console.log(`Error creating issuer: ${error1}`);
+    return res.status(501).send(error1)
   }
 
   // create distributor
-  let distributor
-  try {
-    distributor = await stellarUtils.fundAccount(`distributor${token}`, sponsorSeed)
-  }
-  catch (error) {
-    console.log(`Error creating distributor: ${error}`);
-    return res.status(501).send(error)
+  const [error2, distributor] = await catchify(stellarUtils.fundAccount(`distributor${token}`, sponsorSeed))
+  if (error2) {
+    console.log(`Error creating distributor: ${error2}`);
+    return res.status(501).send(error2)
   }
 
   // create trust line: distributor -> issuer
-  try {
-    await stellarUtils.createTrustLine(token, distributor.seed, issuer.pubk)
-  }
-  catch (error) {
-    console.log(`Error creating trusline: ${error}`)
-    return res.status(501).send(error)
+  const [errorTrustLine] = await catchify(stellarUtils.createTrustLine(token, distributor.seed, issuer.pubk))
+  if (errorTrustLine) {
+    console.log(`Error creating trusline: ${errorTrustLine}`)
+    return res.status(501).send(errorTrustLine)
   }
 
   // authorize trust line: issuer -> distributor
-  try {
-     await stellarUtils.authorizeTrustLine(token, distributor.pubk, issuer.seed)
-  }
-  catch (error) {
-    console.log(`Error authorizing trusline: ${error}`)
-    return res.status(501).send(error)
+  const [errorAuthTrust] = await catchify(stellarUtils.authorizeTrustLine(token, distributor.pubk, issuer.seed))
+  if (errorAuthTrust) {
+    console.log(`Error authorizing trusline: ${errorAuthTrust}`)
+    return res.status(501).send(errorAuthTrust)
   }
 
   // insert issuer
-  try {
-    await queries.insertBankAccount(issuer.pubk, issuer.seed, issuer.name)
-  }
-  catch (error) {
-    console.log(`Error inserting account: ${error}`);
-    return res.status(501).send(error)
+  const [errorInsertAcc1] = await catchify(queries.insertBankAccount(issuer.pubk, issuer.seed, issuer.name))
+  if (errorInsertAcc1) {
+    console.log(`Error inserting account: ${errorInsertAcc1}`);
+    return res.status(501).send(errorInsertAcc1)
   }
 
   // insert distributor
-  try {
-    await queries.insertBankAccount(distributor.pubk, distributor.seed, distributor.name)
-  }
-  catch (error) {
-    console.log(`Error inserting account: ${error}`);
-    return res.status(501).send(error)
+  const [errorInsetAcc2] = await catchify(queries.insertBankAccount(distributor.pubk, distributor.seed, distributor.name))
+  if (errorInsetAcc2) {
+    console.log(`Error inserting account: ${errorInsetAcc2}`);
+    return res.status(501).send(errorInsetAcc2)
   }
 
   // insert token
-  try {
-    await queries.insertToken(token, issuer.pubk, distributor.pubk, amount)
-  }
-  catch (error) {
-    console.log(`Error inserting token: ${error}`);
-    return res.status(501).send(error)
+  const [errorInsertToken] = await catchify(queries.insertToken(token, issuer.pubk, distributor.pubk, amount))
+  if (errorInsertToken) {
+    console.log(`Error inserting token: ${errorInsertToken}`);
+    return res.status(501).send(errorInsertToken)
   }
 
   res.status(200).send(`Token ${token} created`)

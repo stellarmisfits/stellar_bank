72634f3bf77c0352879a4faf1d1291736723a998
gasiortomasz <tomek.mesh@gmail.com>
2019-06-11 14:21:47 +0200

    add freeze unfreeze functionalities

--- issuer-CLI/src/app-unfreeze.js
+++ issuer-CLI/src/app-unfreeze.js
@@ -1,10 +1,35 @@
+const axios = require('axios')
 const program = require('commander');
+const coloros = require('colors')
 
 program
+  .option('-e, --email <email></email>')
   .option('-t, --token <name>', 'token name')
   .parse(process.argv);
 
 if (!program.token) 
   throw new Error('--token arg required')
 
-console.log('not implemented');
+  if (!program.email) 
+  throw new Error('--email arg required')
+
+const {email, token} = program
+
+axios.post(`http://localhost:3005/api/changeAccess`, { email, token, allow: true })
+  .then((response) => {
+    const { status, data } = response
+    console.log(`Status: ${status}`)
+    console.log(`Data: ${data}`)
+
+    if (status !== 200) {
+      console.log(`Error: ${data}`.red)
+      process.exit()
+    }
+
+    if (status === 200) {
+      console.log(`SUCCESS. Freezed user: ${email} for token: ${token}`.green)
+    }
+  }).catch((error) => {
+    console.log(error.response.data)
+    return new Error(`${error}`)
+})
--- issuer-CLI/src/app.js
+++ issuer-CLI/src/app.js
@@ -2,7 +2,6 @@
 
 'use strict'
 
-console.log('dupa');
 const program = require('commander')
 program
   .version('0.0.1')
@@ -10,5 +9,4 @@ program
   .command('ico', 'create Stellar token ICO').alias('i')
   .command('freeze', 'freeze token').alias('f')
   .command('unfreeze', 'unfreeze token').alias('x')
-  .command('balance', 'get asset balance').alias('b')
   .parse(process.argv);
